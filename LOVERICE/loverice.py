ff5e10ac29212da844419bdba9007d45f1b5de25
Yifan0105 <yifanshi0105@gmail.com>
2023-04-14 14:36:41 +0200

    finir toutes flask

--- LOVERICE/loverice.py
+++ LOVERICE/loverice.py
@@ -1,9 +1,12 @@
+from aifc import Error
+
 from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
 import os
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 def create_connection(rep):
     if not os.path.exists(rep):
@@ -17,12 +20,14 @@ def create_connection(rep):
             print(f"The error {e} occured")
     return connection
 
+
 def execute_read_query(connection, query):
     with connection:
         cursor = connection.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
     return result
+
 
 def execute_commit(connection, query, params):
     with connection:
@@ -31,10 +36,77 @@ def execute_commit(connection, query, params):
         connection.commit()
     return
 
+
+@app.route('/')
+def Acceuil():
+    return render_template('Acceuil.html')
+
+
+@app.route('/Acceuil')
+def Acceuil1():
+    return render_template('Acceuil.html')
+
+
+@app.route('/Articles')
+def Articles():
+    return render_template('Articles.html')
+
+
+@app.route('/CasqueBluetooth')
+def CasqueBluetooth():
+    return render_template('CasqueBluetooth.html')
+
+
+@app.route('/ChoixThème')
+def ChoixThème():
+    return render_template('ChoixThème.html')
+
+
+@app.route('/ConsolesDeJeux-Video')
+def ConsolesDeJeuxVideo():
+    return render_template('ConsolesDeJeux-Video.html')
+
+
+@app.route('/EcouteursBluetooth')
+def EcouteursBluetooth():
+    return render_template('EcouteursBluetooth.html')
+
+
+@app.route('/EcranPC')
+def EcranPC():
+    return render_template('EcranPC.html')
+
+
+@app.route('/OrdinateurPortbale')
+def OrdinateurPortbale():
+    return render_template('OrdinateurPortbale.html')
+
+
+@app.route('/Smartphone')
+def Smartphone():
+    return render_template('Smartphone.html')
+
+
+@app.route('/Tablette')
+def Tablette():
+    return render_template('Tablette.html')
+
+
+@app.route('/Thèmes')
+def Thèmes():
+    return render_template('Thèmes.html')
+
+
+@app.route('/TV')
+def TV():
+    return render_template('TV.html')
+
+
 # ---------------Contactez-nous----------------------
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 @app.route('/recu', methods=['GET', 'POST'])
 def recu():
